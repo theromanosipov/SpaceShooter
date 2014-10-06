c7e80215cddbedaa0a166e9e0c8fc7be5908d86c
Kristonitas <Kristupas.stumbrys@gmail.com>
2014-10-06 23:25:37 +0300

    KS testai
    Šiaip buvo pagavęs įkvėpimas kodinti

---
+++ UnityProject/Assets/Scripts/Game.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+	
+	public static Game control;
+	
+	void Awake(){
+		if(control == null){
+			DontDestroyOnLoad(gameObject);
+			control = this;
+		}
+		else if(control != this){
+			Destroy(gameObject);
+		}
+	}
+}
