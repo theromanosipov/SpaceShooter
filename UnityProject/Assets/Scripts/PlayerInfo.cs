36124a90e7b95d63ae1e309731cfcd1a6f83fb93
theromanosipov <theromanosipov@gmail.com>
2014-10-09 12:07:25 +0300

    Pridėjau PlayerInfo klasę
    Saugo informaciją apie konkretų žaidėją: kontrolerio numerį, surinktu
    taškus.
    Leidžia gauti žaidėjo nuspaustus mygtukus ir gauti bei modifikuoti
    taškus.

---
+++ UnityProject/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Saugo informaciją apie konkretų žaidėją: kontrolerio numerį, surinktu taškus.
+/// Leidžia gauti žaidėjo nuspaustus mygtukus ir gauti bei modifikuoti taškus.
+/// </summary>
+public class PlayerInfo : MonoBehaviour {
+
+	public int controllerNumber;
+	private long score = 0;
+	
+	public float GetAxisH() {
+		return Input.GetAxis( "Horizontal" + controllerNumber);
+	}
+	
+	public float GetAxisV() {
+		return Input.GetAxis( "Vertical" + controllerNumber);
+	}
+	
+	public bool GetButtonDock() {
+		return Input.GetButton("Dock" + controllerNumber);
+	}
+	
+	public bool GetButtonPowerup() {
+		return Input.GetButton("Fire" + controllerNumber);
+	}
+	
+	public bool GetButtonMelee() {
+		return Input.GetButton("Melee" + controllerNumber);
+	}
+	
+	public long GetScore() {
+		return score;
+	}
+	
+	public void AddScore( long newScore) {
+		score += newScore;
+	}
+}
