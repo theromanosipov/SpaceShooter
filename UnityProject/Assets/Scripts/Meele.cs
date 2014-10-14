452dc34eb056cc28c4ff5ceb00cde719d598d4f0
Kristonitas <Kristupas.stumbrys@gmail.com>
2014-10-14 22:14:30 +0300

    Added meele functionality, might need fixin

---
+++ UnityProject/Assets/Scripts/Meele.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meele : MonoBehaviour {
+	public int meeleDamage;
+	private PlayerInfo player;
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.transform.parent.GetComponent<PlayerInfo> ();
+	}
+
+	void OnTriggerStay2D (Collider2D other) {
+		if (other.tag != "Enemy") {
+			return;
+				}
+		if (player.GetButtonMelee ()) {
+			other.gameObject.BroadcastMessage("GetDamage", meeleDamage);
+				}
+	}
+}
