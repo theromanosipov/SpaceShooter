d5e2fa3d771064ddaa8fa70a78f00b0931410194
vlskns <vilkaunas@gmail.com>
2014-10-15 21:28:30 +0300

    EnemyShip Nr1.
    Pabandžiau įgyvendinti EnemyShip Nr 1.
    Kurio esmė, kad jis priartėjęs per tam tikrą atstumą pradeda skritsi
    link žaidėjo.
    Gal nelabai skoningai padariau (turbūt vietoj update reiktų fixed
    update).
    Public kintamieji:
    Range - per kokį atstumą priartėjus jis pakeičia savo judėjimo kryptį.
    Speed1 - greitis, kuriuo jis juda prieš priartėjimą.
    Speed2 - greitis, kuriuo jis juda po priartėjimo..
    Damage - kintamasis kurį gal būt naudosim ateityje.
    
    Užmiršau gyvybes pridėti. Bet aš jau šiandien baigiu.

---
+++ UnityProject/Assets/Scripts/Enemies/E_EnemyShipNr1.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class E_EnemyShipNr1 : MonoBehaviour {
+	public float range;
+	public float speed1;
+	public float speed2;
+	public float Damage;
+
+	void Start () {
+		rigidbody.velocity = -transform.forward * speed1;
+	}
+
+	void Update () {		
+		var players=GameObject.FindGameObjectsWithTag("Player");
+
+		GameObject player_0 = null;
+		float minDist = Mathf.Infinity;
+		Vector3 currentPos = transform.position;
+		foreach (GameObject p in players)
+		{
+			float dist = Vector3.Distance(p.transform.position, currentPos);
+			if (dist < minDist)
+			{
+				player_0 = p;
+				minDist = dist;
+			}
+		}
+		if (minDist <= range) {        Vector3 directionOfTravel = player_0.transform.position - currentPos;
+			directionOfTravel.Normalize();
+			rigidbody.velocity = directionOfTravel * speed2;
+
+		} else{		rigidbody.velocity = -transform.forward * speed1;
+				}
+	}
+
+}
