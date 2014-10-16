7a36639489b74aa85ed190f09adc3cace6b7febd
vlskns <vilkaunas@gmail.com>
2014-10-16 12:38:49 +0300

    Šūvių prefab'ai
    Pridėti šūvių prefab'ai su jais judinančiu scriptu.
    Iš esmės orientuota į testavimą.
    Taip pat pridėtas EnemyShipNr0 prefab'as, kuris dar neturi savo judėjimo
    script'o, bet turi ShotSpawn vaiką.

---
+++ UnityProject/Assets/E_Shot_Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class E_Shot_Mover : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = new Vector3(0.0f, 0.0f, -speed);
+
+	}
+
+}
