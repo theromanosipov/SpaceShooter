9cee2d47bcb81681965a2ec165e5ea4b8114d221
Justcer <jcervokas@gmail.com>
2014-10-17 14:09:19 +0300

    SimleMover
    Pridejau plugina judejimui
    
    http://whydoidoit.com/2012/04/06/unity-curved-path-following-with-easing/
    detalesnis aprasymas.

---
+++ UnityProject/Assets/Scripts/Enemies/SimpleMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMover : MonoBehaviour {
+
+
+	public Transform[] path;
+	
+	float t = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Spline.MoveOnPath (path, transform.position, ref t, 0.5f); // Sudelijus path masyva jis vaikscios tuo keliu...
+		
+	}
+}
