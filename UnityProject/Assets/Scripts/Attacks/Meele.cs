1cd3d6ada2f5fd4d00e00e88ee64a3085df35442
Kristonitas <Kristupas.stumbrys@gmail.com>
2014-10-21 23:36:21 +0300

    Melle changed for 3D triggers and prefab

--- UnityProject/Assets/Scripts/Attacks/Meele.cs
+++ UnityProject/Assets/Scripts/Attacks/Meele.cs
@@ -3,19 +3,38 @@ using System.Collections;
 
 public class Meele : MonoBehaviour {
 	public int meeleDamage;
+	public float swingTime = 0.2f;
+	public float cooldown = 0.4f;
 	private PlayerInfo player;
+	private bool right = true;
+	private float pressStart = 0;
 
 	// Use this for initialization
 	void Start () {
 		player = gameObject.transform.parent.GetComponent<PlayerInfo> ();
 	}
 
-	void OnTriggerStay2D (Collider2D other) {
+	void Update(){
+		if (player.GetButtonMelee () && Time.time - pressStart > cooldown + swingTime) {
+			right = !right;
+			pressStart = Time.time;
+		}
+
+		float swangle = (Time.time - pressStart) / swingTime * 120;
+		if(right){
+
+			transform.rotation = Quaternion.Euler(0,Mathf.Clamp(60f + swangle, 60f, 120f),0);
+				}
+		else{
+			transform.rotation = Quaternion.Euler(0,Mathf.Clamp(120f - swangle, 60f, 120f),0);
+		}
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
 		if (other.tag != "Enemy") {
 			return;
 				}
-		if (player.GetButtonMelee ()) {
-			other.gameObject.BroadcastMessage("GetDamage", meeleDamage);
-				}
+		other.gameObject.BroadcastMessage("GetDamage", meeleDamage);
 	}
 }
