d135e5e00b78811f5172f9781798e4959aaa0e2e
vlskns <vilkaunas@gmail.com>
2014-10-23 15:23:08 +0300

    1

--- UnityProject/Assets/Scripts/GenericPlayerController.cs
+++ UnityProject/Assets/Scripts/GenericPlayerController.cs
@@ -12,7 +12,7 @@ public class GenericPlayerController : MonoBehaviour {
         //Jei player == null gaunamas PlayerInfo iš PlayerInfoContainer
         if( player == null) {
             Debug.Log("Getting PlayerInfo");
-		    PlayerInfoContainer newPlayer = gameObject.GetComponent<PlayerInfoContainer>();
+			PlayerInfoContainer newPlayer = gameObject.GetComponentsInParent<PlayerInfoContainer>()[0];
             if( newPlayer == null) {
                 Debug.Log("Failed GenericControllerInfo Update Can't find PlayerInfoContainer");
 		    }
