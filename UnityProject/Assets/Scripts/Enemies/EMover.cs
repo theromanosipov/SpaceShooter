31c53c8fd0bd5d3e6051fb54c8fa7bd21035f8da
vlskns <vilkaunas@gmail.com>
2014-10-23 16:05:16 +0300

    Betvarkė

--- UnityProject/Assets/Scripts/Enemies/EMover.cs
+++ UnityProject/Assets/Scripts/Enemies/EMover.cs
@@ -11,6 +11,7 @@ public class EMover : MonoBehaviour {
 	public float wait;
 	public float mtime;
 	public GameObject shotspawnscenter;
+	public float rotationSpeed;
 	private float p;
 	private float nextmove;
 	private float random;
@@ -20,36 +21,31 @@ public class EMover : MonoBehaviour {
 	{
 		rigidbody.velocity = -speeddown*transform.forward;
 		nextmove += wait;
-		shotspawnscenter.rigidbody.angularVelocity =new Vector3(0,4f,0);
 	}
 
 	void Update () {
-		shotspawnscenter.rigidbody.position = rigidbody.position;
 		if (Time.time >= nextmove) 
 		{
 			StartCoroutine(changevelocity ());
 			nextmove=Time.time+wait;
 
 		}
-
-	}
-
-	void FixedUpdate ()
-	{
 		rigidbody.position = new Vector3 (
 			Mathf.Clamp (rigidbody.position.x, xmin, xmax),
 			0.0f,
 			rigidbody.position.z);
+
 	}
+	void FixedUpdate () {
+		shotspawnscenter.gameObject.transform.Rotate(0.0f,rotationSpeed,0.0f);
+	}
+
 	IEnumerator changevelocity()
 	{
-		//p = (transform.position.x - xmin)/(xmax - xmin);
 		float s;
 		s = transform.position.x;
 		p = 4*((s)*(s)*(s))/((xmax - xmin)*(xmax - xmin)*(xmax - xmin))+0.5f;
 		random = Random.Range (0f,1f);
-//		Debug.Log (p);
-		//Debug.Log (random);
 		if (random < p) {rigidbody.velocity += new Vector3(-hspeed,0f,0f);}
 		else {rigidbody.velocity += new Vector3(hspeed,0f,0f);}
 		yield return new WaitForSeconds(mtime);		
