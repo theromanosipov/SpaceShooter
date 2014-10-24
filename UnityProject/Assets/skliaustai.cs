c89346091a286161159be83392ae2ad841664a17
vlskns <vilkaunas@gmail.com>
2014-10-24 15:51:19 +0300

    ES5 prefab update

---
+++ UnityProject/Assets/skliaustai.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skliaustai : MonoBehaviour {
+
+	// Use this for initialization
+	public float speedVertical;
+	public float HorizontaloConst;
+	public float time0;
+	private float starttime;	
+	void Start () {
+		starttime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float vel = -HorizontaloConst/100+HorizontaloConst * ((Time.time - starttime) - time0) * ((Time.time - starttime) - time0) * ((Time.time - starttime) - time0)/10;
+		rigidbody.velocity = new Vector3(-vel,0.0f,speedVertical);
+	}
+}
