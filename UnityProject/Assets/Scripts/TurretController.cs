8a42f8424e017266a142eac57c2274e973b22b21
vlskns <vilkaunas@gmail.com>
2014-10-26 14:25:16 +0200

    TurretController Beta.
    TurretController script'as, kuris turėtų susitvarkyti tiek su fiksuotais
    bokšteliais, tiek su tais, kurie sukasi aplink laivą.
    
    Kintamieji:
    public:
    speed -> kokiu greičiu sukasi.
    distance -> kokiu atstumu sukasi (jei bokštelis fiksuotas = 0)
    minAngle ir maxAngle -> Jei bokštelis fiksuotas (isFixed=true), tada
    script'as neleis bokšteliui suktis daugiau nei šitos vertės.
    
    Script'o netestavau dar, tai gali būti problemų su sukimusi :?... Vėliau
    bandysiu pratestuoti...

---
+++ UnityProject/Assets/Scripts/TurretController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretController : GenericPlayerController {
+
+	public float speed;
+	public float distance;
+	private float angle;
+	private bool isFixed;
+	public float minAngle;
+	public float maxAngle;
+	void Start() {
+		angle = 0;
+		isFixed = false;
+	}
+	void FixedUpdate () {
+		angle += 180f/3.14f*speed * player.GetAxisH ();
+		if (isFixed) {
+			Mathf.Clamp (angle,minAngle,maxAngle);
+				}
+		transform.position = transform.parent.position+distance*new Vector3 (Mathf.Sin (angle), 0.0f, Mathf.Cos (angle));
+		transform.rotation = Quaternion.Euler (0.0f, angle, 0.0f);
+	}
+}
