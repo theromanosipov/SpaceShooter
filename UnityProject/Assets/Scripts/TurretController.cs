b38f510913d3b4ecd42fbe8636cb3ce7fe54b708
vlskns <vilkaunas@gmail.com>
2014-10-27 12:14:49 +0200

    TurretController

--- UnityProject/Assets/Scripts/TurretController.cs
+++ UnityProject/Assets/Scripts/TurretController.cs
@@ -6,19 +6,26 @@ public class TurretController : GenericPlayerController {
 	public float speed;
 	public float distance;
 	private float angle;
-	private bool isFixed;
-	public float minAngle;
-	public float maxAngle;
+	private bool isRotationClamped;	
+	private float minAngle;
+	private float maxAngle;
 	void Start() {
 		angle = 0;
-		isFixed = false;
+		isRotationClamped = false;
 	}
 	void FixedUpdate () {
 		angle += 3.14f/180f*speed * player.GetAxisH ();
-		if (isFixed) {
+		if (isRotationClamped) {
 			Mathf.Clamp (angle,minAngle,maxAngle);
 				}
 		transform.position = transform.parent.position+distance*new Vector3 (Mathf.Sin (angle), 0.0f, Mathf.Cos (angle));
 		transform.rotation = Quaternion.Euler (0.0f, angle, 0.0f);
 	}
+
+	public void RestrictRotation(float minAngle, float maxAngle) {
+		isRotationClamped = true;
+		this.minAngle = minAngle;
+		this.maxAngle = maxAngle;
+	}
+
 }
