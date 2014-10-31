16a92b404f5b065f73c8f4cc57be946ac51a478f
vlskns <vilkaunas@gmail.com>
2014-10-31 21:36:25 +0200

    Spalvos pakitimas gavus šūvį.

--- UnityProject/Assets/Scripts/Enemies/E_EnemyInfo.cs
+++ UnityProject/Assets/Scripts/Enemies/E_EnemyInfo.cs
@@ -12,10 +12,23 @@ public class E_EnemyInfo : MonoBehaviour {
 	// Use this for initialization
 	public int HitPoints;		//Laivo gyvybės.
 	public int ContactDamage;	//Kiek žalos daro žaidėjui kontakto metu
-	
+	public float colorResetTime;
+	private float resetTime;
+
+	void Start()
+	{
+		colorResetTime = 0.3f;
+	}
+
+	void Update()
+	{
+		if (Time.time >= resetTime) {gameObject.renderer.material.color = new Color(1f,0.7f,0.7f);}
+	}
 	// Update is called once per frame
 	void GetDamage(int Damage)
 	{
+		gameObject.renderer.material.color = new Color(1f,0,0);
+		resetTime = Time.time + colorResetTime;
 		HitPoints -= Damage;
 		if (HitPoints <= 0) {
 			DestroyShip ();
