58f57f4c8b93f05149fd8076eae126dda5c08654
vlskns <vilkaunas@gmail.com>
2014-11-07 19:32:06 +0200

    Dar keli bug'ai

--- UnityProject/Assets/2D scripts/Enemies/x2D_EnemySeekingShip.cs
+++ UnityProject/Assets/2D scripts/Enemies/x2D_EnemySeekingShip.cs
@@ -10,7 +10,7 @@ public class x2D_EnemySeekingShip : MonoBehaviour {
 	public float speed2; //kokiu greičiu juda, kai pastebi žaidėją.
 	
 	void Start () {
-		rigidbody2D.velocity = -transform.up * speed1;
+		transform.rotation = Quaternion.Euler (0, 0, 180);
 	}
 	
 	void Update () {		
@@ -34,7 +34,7 @@ public class x2D_EnemySeekingShip : MonoBehaviour {
 			transform.rotation= Quaternion.Euler(0,0,-90+(180/3.14159f)*Mathf.Atan(rigidbody2D.velocity.y/rigidbody2D.velocity.x));
 			
 		} else{	
-			rigidbody2D.velocity = -transform.up * speed1;
+			rigidbody2D.velocity = transform.up * speed1;
 		}
 	}
 }
