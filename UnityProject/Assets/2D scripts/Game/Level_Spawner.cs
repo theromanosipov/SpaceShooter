b5fbfb7eab82bf1daaec3092c6561a174a39e9f5
vlskns <vilkaunas@gmail.com>
2014-11-16 16:26:29 +0200

    LevelSpawner
    SpawnRow metodas

--- UnityProject/Assets/2D scripts/Game/Level_Spawner.cs
+++ UnityProject/Assets/2D scripts/Game/Level_Spawner.cs
@@ -13,59 +13,71 @@ public class Level_Spawner : MonoBehaviour
 	private float nextWave=0;
 	private int timerTracker=0;
 		
-		void Start ()
-		{
-		for (int i=0; i<isLevelEnabled.Length; i++)
-						isLevelEnabled[i] = true;
+	void Start ()
+	{
 		nextWave = Time.time + timers [timerTracker];
-		}
-		// Update is called once per frame
-		void FixedUpdate ()
+	}
+
+	void FixedUpdate ()
+	{
+		if (Time.time > nextWave&&timerTracker < timers.Length) 
 		{
-		if (Time.time > nextWave&&timerTracker < timers.Length) {
-						gameObject.SendMessage ("Level" + timerTracker);
-						timerTracker++;
-						nextWave=Time.time+timers[timerTracker];
-				}
+			gameObject.SendMessage ("Level" + timerTracker);
+			timerTracker++;
+			nextWave=Time.time+timers[timerTracker];
 		}
-		IEnumerator Level0 ()
+	}
+	
+	void SpawnRow(GameObject prefab, int count)
+	{
+		count--;
+		for (int i=0; i<=count; i++)
 		{
+			Instantiate (prefab, new Vector3(XValues[0]+1+i*(XValues[1]-XValues[0]-2)/count,YValue,0), Quaternion.identity);
+		}
+	}
+
+	IEnumerator Level0 ()
+	{
+		isLevelEnabled [0] = true;
 		float delay=0.4f;	
-		while (!gameOver&&isLevelEnabled[0]) {
-				Instantiate (enemyPrefabs[0], new Vector3(Random.Range(XValues[0],XValues[1]),YValue,0), Quaternion.identity);
-				yield return new WaitForSeconds(delay);
-		}
-		}
-		IEnumerator Level1 ()
+		while (!gameOver&&isLevelEnabled[0]) 
 		{
+			Instantiate (enemyPrefabs[0], new Vector3(Random.Range(XValues[0],XValues[1]),YValue,0), Quaternion.identity);
+			yield return new WaitForSeconds(delay);
+		}
+	}
+
+	IEnumerator Level1 ()
+	{
+		isLevelEnabled [1] = true;
 		float delay=3f;	
 		int shieldCount=15;
 		Instantiate (enemyPrefabs[3], new Vector3(XValues[0]/2,YValue,0), Quaternion.identity);
 		Instantiate (enemyPrefabs[3], new Vector3(XValues[1]/2,YValue,0), Quaternion.identity);
-		for (int i=0; i<=shieldCount; i++)
+		SpawnRow (powerupPrefabs [4], shieldCount);
+		while (!gameOver&&isLevelEnabled[1]) 
 		{
-			Instantiate (powerupPrefabs[4], new Vector3(XValues[0]+0.5f+i*(XValues[1]-XValues[0]-1)/shieldCount,YValue,0), Quaternion.identity);
-		}
-		while (!gameOver&&isLevelEnabled[1]) {
 			Instantiate (enemyPrefabs[1], new Vector3(Random.Range(XValues[0],XValues[1]),YValue,0), Quaternion.identity);
 			yield return new WaitForSeconds(delay);
 		}
-		}
-		void Level2()
-		{
+	}
+		
+	void Level2()
+	{
+		isLevelEnabled [2] = true;
+
 		isLevelEnabled [0] = false;
 		isLevelEnabled [1] = false;
 		Instantiate (enemyPrefabs[4], new Vector3(XValues[0]/2,YValue,0), Quaternion.identity);
 		Instantiate (enemyPrefabs[4], new Vector3(XValues[1]/2,YValue,0), Quaternion.identity);
-		}
+	}
 
-		void Level3()
-		{
-		isLevelEnabled [0] = true;
-		isLevelEnabled [1] = true;
-		Debug.Log ("AAAAAAAAAAAAAAAAAA");
+	void Level3()
+	{
+		isLevelEnabled [3] = true;
 		gameObject.SendMessage ("Level0");
 		gameObject.SendMessage ("Level1");
-		}
+	}
 }
 
