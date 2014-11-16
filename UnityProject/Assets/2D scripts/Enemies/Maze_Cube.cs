8892d055516d8fd4780ba2756e92fda538b1e8f6
vlskns <vilkaunas@gmail.com>
2014-11-16 21:50:18 +0200

    Justui ->

---
+++ UnityProject/Assets/2D scripts/Enemies/Maze_Cube.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze_Cube : MonoBehaviour {
+	public int damage = 10000;
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Shot")
+						Destroy (other.gameObject);
+		if (other.tag == "Enemy" || other.tag == "Player")
+						other.SendMessage ("GetDamage", damage);
+
+	}
+}
