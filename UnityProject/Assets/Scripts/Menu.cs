9425d162c9144977ab4ed7701af850659ff4d12f
Kristonitas <Kristupas.stumbrys@gmail.com>
2014-11-23 23:25:54 +0200

    Menu sucentruotas

--- UnityProject/Assets/Scripts/Menu.cs
+++ UnityProject/Assets/Scripts/Menu.cs
@@ -27,7 +27,7 @@ public class Menu : MonoBehaviour {
 	
 	void MainPage()
 	{
-		GUILayout.BeginArea(new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height));
+		GUILayout.BeginArea(new Rect(Screen.width / 4, 0, Screen.width / 2, Screen.height));
 		{
 			if (GUILayout.Button("Play Game"))
 			{
@@ -39,13 +39,13 @@ public class Menu : MonoBehaviour {
 			currentPage = Page.Credits;
 		}
 		GUILayout.EndArea ();
-		GUI.Label (new Rect (0, Screen.height * 9 / 10, Screen.width / 3, Screen.height / 10), "VERZYJA:0.0.0.0.0.0.0.1");
+		GUI.Label (new Rect (Screen.width / 3, Screen.height * 9 / 10, Screen.width / 3, Screen.height / 10), "VERZYJA:0.0.0.0.0.0.0.1");
 	}
 	
 	void CreditsPage(){
-		GUI.Label(new Rect(0, Screen.height / 2, Screen.width, Screen.height / 2),
-		          "Space Joint by Free");
-		if(GUI.Button(new Rect(Screen.width / 2,  Screen.height * 3 / 8, Screen.width / 4, Screen.height / 8), "Back")){
+		GUI.Label(new Rect(Screen.width / 4, Screen.height / 2, Screen.width / 2, Screen.height / 2),
+		          "TBD by Free");
+		if(GUI.Button(new Rect(Screen.width / 4,  Screen.height * 3 / 8, Screen.width / 2, Screen.height / 8), "Back")){
 			currentPage = Page.Main;
 		}
 	}	
