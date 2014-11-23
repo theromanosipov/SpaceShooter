cce1033c36b80587c5aa55205309beb62852de50
theromanosipov <theromanosipov@gmail.com>
2014-11-23 23:24:45 +0200

    Blah blah blah

--- UnityProject/Assets/2D scripts/Enemies/PointArrayMover.cs
+++ UnityProject/Assets/2D scripts/Enemies/PointArrayMover.cs
@@ -8,24 +8,29 @@ using System.Collections;
 /// </summary>
 public class PointArrayMover : MonoBehaviour {
 
-	public Vector2[] destinations;                  // Taškai per kūriuos keliaus
     public Vector2 startingPosition;                // Pozicija paskutinio update metu, prieš pirmąjį update pradinė pozicija
+	public Vector2[] destinations;                  // Taškai per kūriuos keliaus
+    public float duration;                          // Kiek kartų bus ištempta speed animacija
     public AnimationCurve speed;                    // Greičio ir laiko priklausomybė
 
     private Vector2 currentDestination;
     private int currentDestinationNumber;
+
+    private float startTime;
 
     void Start ()
     {
         transform.position = new Vector3(startingPosition.x, startingPosition.y, 0);
         currentDestinationNumber = 0;
         currentDestination = destinations[currentDestinationNumber];
+
+        startTime = Time.time;
     }
 	
 	void Update () 
     {
         // Pajudina link currentDestination
-        startingPosition = Vector2.MoveTowards(startingPosition, currentDestination, speed.Evaluate(0.5f));
+        startingPosition = Vector2.MoveTowards(startingPosition, currentDestination, speed.Evaluate((Time.time - startTime) / duration));
         transform.position = new Vector3(startingPosition.x, startingPosition.y, 0);
 
         if (currentDestinationNumber == destinations.Length - 1 && startingPosition == currentDestination)  // Pasiekėme paskutinį destination
