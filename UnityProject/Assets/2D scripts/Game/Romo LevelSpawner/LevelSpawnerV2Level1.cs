5fea4583bb0cdfd9ccdfb7af63325ff9d2d9dc58
theromanosipov <theromanosipov@gmail.com>
2014-11-27 21:06:56 +0200

    Plokštelė

--- UnityProject/Assets/2D scripts/Game/Romo LevelSpawner/LevelSpawnerV2Level1.cs
+++ UnityProject/Assets/2D scripts/Game/Romo LevelSpawner/LevelSpawnerV2Level1.cs
@@ -5,9 +5,9 @@ public class LevelSpawnerV2Level1 : LevelSpawnerV2 {
 
 	IEnumerator WaveSpinners( int currentBeat) {
         Vector2[] destination = new Vector2[2];
-        destination[0] = new Vector2(28.9f, 16.1f);
+        destination[0] = new Vector2(27.1f, 15.38f);
         destination[1] = new Vector2(-30, -18);
-        int delay = 8;
+        int delay = 1;
 
         while (!isGameOver && currentBeat + delay <= beatTime.Length) {
             GameObject currentEnemy = Instantiate(enemyPrefabs[0]) as GameObject;
